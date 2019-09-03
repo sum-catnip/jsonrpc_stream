81197289f4673a894277d9a73a54eef8a5fb2abd
sum catnip <catnip@catnip.fyi>
2019-09-03 21:58:22 +0200

    remade the decorators so they actually make sense

--- jsonrpc_stream/dispatcher.py
+++ jsonrpc_stream/dispatcher.py
@@ -12,22 +12,53 @@ class DiscoverMode(enum.Enum):
     all       = enum.auto()
 
 
-def dispatch_target(name: typing.Optional[str] = None) -> typing.Callable:
-    if callable(name):
-        name.__dict__['__jsonrpc_dispatch'] = name.__name__
-        return name
+class RequestType(enum.Enum):
+    request = enum.auto()
+    notification = enum.auto()
 
-    def decorator(method: typing.Callable) -> typing.Callable:
-        method.__dict__['__jsonrpc_dispatch'] = name
-        return method
-    return decorator
+
+class DecoratedTarget:
+    name: str
+    type_: RequestType
+
+    def __init__(self, name: str, type_: RequestType):
+        self.name = name
+        self.type_ = type_
+
+
+def mark_method(
+    target: typing.Callable,
+    name: typing.Optional[str],
+    type_: RequestType
+):
+    setattr(
+        target,
+        '__jsonrpc__',
+        DecoratedTarget(name or target.__name__, type_)
+    )
+
+
+def request(
+    method: typing.Callable = None, name: str = None
+) -> typing.Callable:
+    if not callable(method): return functools.partial(request, name=method)
+    mark_method(method, name, RequestType.request)
+    return method
+
+
+def notification(
+    method: typing.Callable = None, name: str = None
+) -> typing.Callable:
+    if not callable(method): return functools.partial(request, name=method)
+    mark_method(method, name, RequestType.notification)
+    return method
 
 
 class DispatchNamespace:
     def __init__(self, obj: typing.Any, mode: DiscoverMode):
         if mode == DiscoverMode.decorated:
             def istarget(obj: typing.Any):
-                return hasattr(obj, '__jsonrpc_dispatch')
+                return hasattr(obj, '__jsonrpc__')
         elif mode == DiscoverMode.public:
             def istarget(obj: typing.Any):
                 return callable(obj) and not obj.__name__.startswith('_')
@@ -35,41 +66,38 @@ class DispatchNamespace:
             def istarget(obj: typing.Any): return callable(obj)
 
         targets = inspect.getmembers(obj, predicate=istarget)
-        self.targets: typing.Dict[str, typing.Callable] = {}
+        self.notifications: typing.Dict[str, typing.Callable] = {}
+        self.requests: typing.Dict[str, typing.Callable] = {}
         for name, target in targets:
-            name = getattr(target, '__jsonrpc_dispatch', name)
+            mark = getattr(
+                target,
+                '__jsonrpc__',
+                DecoratedTarget(name, RequestType.request)
+            )
             if not inspect.iscoroutinefunction(target):
-                async def async_wrapper(*args, **kwargs):
-                    return target(*args, **kwargs)
-                self.targets[name] = async_wrapper
-            else: self.targets[name] = target
+                t = target
 
-    async def call(self, method: str, *args, **kwargs):
-        try: return await self.targets[method](*args, **kwargs)
+                async def async_wrapper(*args, **kwargs):
+                    return t(*args, **kwargs)
+                target = async_wrapper
+            if mark.type_ == RequestType.request:
+                self.requests[mark.name] = target
+            elif mark.type_ == RequestType.notification:
+                self.notifications[mark.name] = target
+
+    async def notify(self, method: str, *args, **kwargs):
+        try: await self.notifications[method](*args, **kwargs)
         except KeyError:
             raise exceptions.JsonRpcMethodNotFound.from_method(method)
         except TypeError:
             raise exceptions.JsonRpcInvalidParams.from_method(method)
 
-    async def call_paramsobj(self, method: str, params: typing.Any):
-        if isinstance(params, list):
-            return await self.call(method, *params)
-        elif isinstance(params, dict):
-            return await self.call(method, **params)
-        elif params is None:
-            return await self.call(method)
-        else: return await self.call(method, params)
-
-
-def proxy_target(name: typing.Optional[str] = None) -> typing.Callable:
-    if callable(name):
-        name.__dict__['__jsonrpc_proxy'] = name.__name__
-        return name
-
-    def decorator(method: typing.Callable) -> typing.Callable:
-        method.__dict__['__jsonrpc_proxy'] = name
-        return method
-    return decorator
+    async def call(self, method: str, *args, **kwargs):
+        try: return await self.requests[method](*args, **kwargs)
+        except KeyError:
+            raise exceptions.JsonRpcMethodNotFound.from_method(method)
+        except TypeError:
+            raise exceptions.JsonRpcInvalidParams.from_method(method)
 
 
 class ProxyNamespace:
@@ -78,12 +106,13 @@ class ProxyNamespace:
         name:     str,
         obj:      typing.Any,
         mode:     DiscoverMode,
-        callback: typing.Callable
+        callback_request: typing.Callable,
+        callback_notify: typing.Callable
     ):
         self.name = name
         if mode == DiscoverMode.decorated:
             def istarget(obj: typing.Any):
-                return hasattr(obj, '__jsonrpc_proxy')
+                return hasattr(obj, '__jsonrpc__')
         elif mode == DiscoverMode.public:
             def istarget(obj: typing.Any):
                 return callable(obj) and not obj.__name__.startswith('_')
@@ -95,11 +124,27 @@ class ProxyNamespace:
         self.targets: typing.Dict[str, typing.Callable] = {}
 
         for name, target in targets:
-            name = getattr(target, '__jsonrpc_proxy', name)
+            mark = getattr(
+                target,
+                '__jsonrpc__',
+                DecoratedTarget(name, RequestType.request)
+            )
 
-            def capture(name=name):
+            def capture(mark=mark):
                 @functools.wraps(target)
-                async def wrapper(*args, **kwargs):
-                    return await callback(self.name, name, *args, **kwargs)
-                return wrapper
+                async def request_wrapper(*args, **kwargs):
+                    return await callback_request(
+                        self.name, mark.name, *args, **kwargs
+                    )
+
+                @functools.wraps(target)
+                async def notify_wrapper(*args, **kwargs):
+                    return await callback_notify(
+                        self.name, mark.name, *args, **kwargs
+                    )
+
+                if mark.type_ == RequestType.request: return request_wrapper
+                elif mark.type_ == RequestType.notification:
+                    return notify_wrapper
+
             setattr(obj, target.__name__, capture())
